65de8708b6512e722e528e3e95b19232155a6ac0
unknown <thiago.barreto.oliveira22@gmail.com>
2023-02-25 22:30:04 -0300

    Criando variáveis de ambiente, e conexão com o MongoDB

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # API RESTful com Node.js e MongoDB
 
 CRUD com Node, Express e Mongoose
+
+Crie um banco de dados no MongoDB ou MongoDB Atlas
+
+Crie um arquivo .env e insira as variáveis de ambiente DB_USER e DB_PASSWORD com as respectivas credenciais.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 app.use(
     express.urlencoded({
@@ -16,4 +20,12 @@ app.get("/", (req, res) => {
     });
 });
 
-app.listen(3000);
+mongoose
+    .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.qxgsfsr.mongodb.net/APIdb?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        console.log("Conexão estabelecida com o banco de dados!");
+        app.listen(3000);
+    })
+    .catch((err) => console.log(err));
