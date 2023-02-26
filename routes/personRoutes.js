4136a6ae29e3260fef622b141fb9c84edafafc1f
unknown <thiago.barreto.oliveira22@gmail.com>
2023-02-26 00:45:33 -0300

    UPDATE com PATCH na rota /person/{ID}

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -65,4 +65,29 @@ router.get("/:id", async (req, res) => {
     }
 });
 
+router.patch("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const { name, salary, approved } = req.body;
+
+    const person = {
+        name,
+        salary,
+        approved,
+    };
+
+    try {
+        const updatedPerson = await Person.updateOne({ _id: id }, person);
+
+        if (updatedPerson.matchedCount === 0) {
+            res.status(422).json({ message: "O usuário não foi encontrado!" });
+            return;
+        }
+
+        res.status(200).json(person);
+    } catch {
+        res.status(500).json({ error: error });
+    }
+});
+
 module.exports = router;
